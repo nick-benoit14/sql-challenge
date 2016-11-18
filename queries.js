af414a3d4cd2e68bd4b488b85cc15e7019d2e38a
Nick <nick.benoit14@gmail.com>
2016-11-17 21:52:45 -0700

    working query

--- queries.js
+++ queries.js
@@ -1,9 +1,19 @@
 var pgp = require('pg-promise')();
 var connectionString = 'postgres://localhost:5432/posts';
-var db = pgp(connectionString);
+var db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'blog',
+    user: 'nickbenoit',
+    password: 'Atomicasdf'
+});
 
 function getAllPosts(cb) {
-  cb([{title:"Fake Title", content: "Fake Content"}]);
+  db.any('select * from posts').then(function(posts){
+    cb(posts);
+  }).catch(function(err){
+    cb(err);
+  })
 }
 
 module.exports = {
